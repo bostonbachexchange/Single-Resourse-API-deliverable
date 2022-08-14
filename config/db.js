4a76c5dd67d0543fd11c295d99a36797b7347f13
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-08-14 14:10:32 -0400

    updates for deployment

--- config/db.js
+++ config/db.js
@@ -18,4 +18,6 @@ const localDb = process.env.TESTENV ? database.test : database.development
 // heroku production evironment otherwise use test or development db
 const currentDb = process.env.MONGODB_URI || localDb
 
+// mongodb+srv://jclapper:IfTheWheelsFallOffWeRideTheHubCaps@projectcrud.hvhcg.mongodb.net/repertoire?retryWrites=true&w=majority
+
 module.exports = currentDb
